87462901ea08f83a8d4a3662100857b158a21887
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2024-06-26 11:58:56 +0300

    Create 785A.cpp

---
+++ 785A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, total_faces = 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    string s;
+    cin >> s;
+
+    if (s == "Tetrahedron")
+      total_faces += 4;
+
+    else if (s == "Cube")
+
+      total_faces += 6;
+
+    else if (s == "Octahedron")
+      total_faces += 8;
+
+    else if (s == "Dodecahedron")
+
+      total_faces += 12;
+
+    else if (s == "Icosahedron")
+      total_faces += 20;
+  }
+  cout << total_faces;
+
+  return 0;
+}
