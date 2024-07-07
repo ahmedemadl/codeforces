3d82fa0b464c25d212fb09af962ffa69fed83d41
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2024-07-07 22:02:59 +0300

    Create 688B.cpp

---
+++ 688B.cpp
@@ -0,0 +1,23 @@
+    //=====================================================
+    // Author      : ahmedgbr367
+    // Date        : 2024-07-06 16:55:18
+    // Description : Junior Training Sheet V7.0
+    // Problem Code: 688B
+    // Problem Name: B_Lovely_Palindromes
+    // Learned     : 
+    //=====================================================
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main(){
+        cin.tie(0)->sync_with_stdio(0);
+        string s;
+        cin >> s;
+        cout<< s;
+        for (int i=(s.length()-1) ;i>=0;i--){
+            cout << s[i];
+     
+        } 
+     
+        return 0;
+    }
