8278b8941ff5d2e65cc65063327ec8632bc1a32b
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2024-07-17 22:54:11 +0300

    Create Assiut,sheet#2: (O).cpp

---
+++ Assiut,sheet#2: (O).cpp
@@ -0,0 +1,22 @@
+//=====================================================
+// Author      : ahmedgbr367
+// Date        : 29/6/2024
+// Description : Assiut University Training
+// Problem Type: sheet 2 (O)
+// Problem No. : 219432
+//=====================================================
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n,m=0;
+  cin >> n;
+  for (int i = 0 ; i < n ; i++){
+    m++;
+    for (int j = 0 ; j < m ;j++ ){
+    cout << "*";
+    }
+   cout << endl;
+  }
+    return 0;
+}
