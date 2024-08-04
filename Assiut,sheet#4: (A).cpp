5df3beba301e5dba6452fe42c326b71399c204b3
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2024-08-04 23:45:18 +0300

    Create Assiut,sheet#4: (A).cpp

---
+++ Assiut,sheet#4: (A).cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string S1 , S2; 
+  cin >> S1 >> S2;
+  cout << S1.length() << " "<<  S2.length() << endl << S1 << " " << S2; ;
+    return 0;
+}
