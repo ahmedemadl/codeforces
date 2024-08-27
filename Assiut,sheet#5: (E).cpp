6eed20d128ac59b161e07be2aae19a76f39344d1
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2024-08-27 23:50:57 +0300

    Create Assiut,sheet#5: (E).cpp

---
+++ Assiut,sheet#5: (E).cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void Swap( int x , int y){
+    cout << y <<" "<< x;
+}
+int main() {
+    int x, y;
+    cin >> x >>y;
+    Swap(x,y);
+    
+    return 0;
+}
