637c76e71ea56274bc979a63ebf497916500bbee
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2024-10-20 22:56:16 +0300

    Create Assiut,sheet#7: (G).cpp

---
+++ Assiut,sheet#7: (G).cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Recursion(short N, int i, short empty_space){
+  
+    if( N == 0 ){ // Base case
+        return;
+    }
+
+    Recursion(N - 1, i - 2,empty_space+1);
+    for(short j = 1 ; j <= (empty_space) ; j++){
+        cout << " ";
+    }
+    for(short j = 1 ; j <= i; j++){
+        cout << "*";
+    }
+    cout << endl;
+
+}
+int main(){
+    short N;
+    cin >> N;
+    Recursion(N, (N*2)-1, 0);
+}
