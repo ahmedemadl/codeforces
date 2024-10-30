734e788d087956210a4c85105916e2c8df00ff65
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2024-10-31 00:09:47 +0300

    Update 510A.cpp

--- 510A.cpp
+++ 510A.cpp
@@ -1,31 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
-
-    int n, m;
-    cin >> n >> m;
-
-    bool check = 0;
-    
-    for (int i = 0; i < n; i++) {
-        if (i % 2 == 0) {
-            for (int j = 0; j < m; j++) {
-                cout << "#";
+    //=====================================================
+    // Author      : ahmedgbr367
+    // Date        : 2024-10-30 23:47:26
+    // Description : Junior Training Sheet V7.0
+    // Problem Code: 510A
+    // Problem Name: A_Fox_And_Snake
+    // Learned     : 
+    //=====================================================
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+     
+    int main(){
+        cin.tie(0)->sync_with_stdio(0);
+        int n , m;
+        cin >> n >> m;
+        int check = 0;
+        for(int i = 0 ; i < n ; i++ ){
+           if( i % 2 != 0){
+            check++;
+           }
+            for(int j = 0 ; j < m ; j++){
+                if(i % 2 == 0){
+                cout << "#"; 
+                }
+                else{
+                    
+                    if(j == m-1 && check % 2 != 0){
+                        cout << "#";
+                    }
+                    else if(j == 0 && check % 2 == 0){
+                        cout << "#";
+                    }
+                    else{
+                    cout << ".";
+                    }
+                }
             }
-        } else {
-            if (!check) {
-                for (int j = 0; j < m - 1; j++) cout << ".";
-                cout << "#";
-            } else {
-                cout << "#";
-                for (int j = 1; j < m; j++) cout << ".";
-            }
-            check = !check;
+            cout << endl;
         }
-        cout << '\n';
+     
+        return 0;
     }
 
-    return 0;
-}
