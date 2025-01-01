f2e55726d7251b6dc1d71725b3792cde81e5b246
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-01-01 18:48:33 +0200

    Create 4A.cpp

---
+++ 4A.cpp
@@ -0,0 +1,25 @@
+//=====================================================
+// Author      : ahmedgbr367
+// Date        : 2025-01-01 18:34:42
+// Description : Junior Training Sheet V7.0
+// Problem Code: 4A
+// Problem Name: A_Watermelon
+// Learned     : 
+//=====================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    short w;
+    cin >> w;
+    if (w % 2 == 0 && w > 2){
+        cout << "YES"; 
+    }
+    else{
+        cout << "NO";
+    }
+
+    return 0;
+}
