7ee8977f8e59e261d7a139cec4bfbc349c539769
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-01-17 23:52:58 +0200

    Create 221A.cpp

---
+++ 221A.cpp
@@ -0,0 +1,24 @@
+    //=====================================================
+    // Author      : ahmedgbr367
+    // Date        : 2025-01-17 23:50:36
+    // Description : Junior Training Sheet V7.0
+    // Problem Code: 221A
+    // Problem Name: A_Little_Elephant_and_Function
+    // Learned     : 
+    //=====================================================
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+     
+    int main(){
+        cin.tie(0)->sync_with_stdio(0);
+     
+        int n;
+        cin >> n;
+        cout << n << " ";
+        for (int i = 1; i < n; ++i) {
+            cout << i << " ";
+        }
+        cout << endl;
+        return 0;
+    }
