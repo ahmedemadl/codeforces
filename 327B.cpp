6f8cac23799fe76983487ce4ee9256d6d539311f
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-02-19 21:09:12 +0200

    Create 327B.cpp

---
+++ 327B.cpp
@@ -0,0 +1,22 @@
+//=====================================================
+// Author      : ahmedgbr367
+// Date        : 2025-02-19 21:00:22
+// Description : Junior Training Sheet V7.0
+// Problem Code: 327B
+// Problem Name: B_Hungry_Sequence
+// Learned     : 
+//=====================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    for(int i = n ; i < 2*n ; i++){
+        cout << i << " ";
+    }
+
+    return 0;
+}
