2897d8c7dc6ca7609c2a8b8aa4a313f749b0ea9b
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-03-10 00:09:18 +0200

    Update 534A.cpp

--- 534A.cpp
+++ 534A.cpp
@@ -6,21 +6,32 @@ int main() {
     cin >> n;
 
     vector<int> result;
-
-    // First, add all odd numbers
+    if(n == 1 || n == 2){
+        cout << 1 << endl << 1;
+        return 0;
+    }
+    else if(n == 3){
+        cout << 2 << endl << 1 << " " << 3 ;
+        return 0;
+    }
+    else if(n == 4){
+        cout << 4 << endl << 3 << " "<< 1 << " " << 4 << " " << 2;
+        return 0;
+    }
+  
     for (int i = 1; i <= n; i += 2) {
         result.push_back(i);
     }
 
-    // Then, add all even numbers
+   
     for (int i = 2; i <= n; i += 2) {
         result.push_back(i);
     }
 
-    // Output the number of students
+   
     cout << result.size() << endl;
 
-    // Output the arrangement
+   
     for (int i = 0; i < result.size(); i++) {
         cout << result[i] << " ";
     }
