0cc87287a51a255a24ec5c0de1d980653d53bfdb
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-05-10 22:59:02 +0300

    Create 165B.cpp

---
+++ 165B.cpp
@@ -0,0 +1,42 @@
+//=====================================================
+// Author      : ahmedgbr367
+// Date        : 2025-05-10 16:17:47
+// Description : Junior Training Sheet V7.0
+// Problem Code: 165B
+// Problem Name: B_Burning_Midnight_Oil
+// Learned     : binary search
+//=====================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll n , k;
+int can( ll mid){
+    ll sum = 0, i = 0;
+    while(true){       
+        ll num = mid/pow(k,i);
+        sum +=num;
+        i++;
+        if(num == 0){
+            break;
+        }
+    }
+    return sum>=n;
+}
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cin>>n>>k;
+    ll l=1,r=n, mid,ans;
+    while(l<=r){
+        mid = l+((r-l)/2); // شغل عالى عشان avoid overflow اصحى للكلام
+        if(can(mid)){
+            r=mid-1;
+            ans=mid;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    cout <<ans;
+    return 0;
+}
