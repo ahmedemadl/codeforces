68516bfa261893733cac5a57008e45c76faffaeb
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-05-21 21:25:26 +0300

    Create _Mahmoud_Ayman,sheet#12: (G).cpp

---
+++ _Mahmoud_Ayman,sheet#12: (G).cpp
@@ -0,0 +1,20 @@
+//=====================================================
+// Author      : ahmedgbr367
+// Date        : 2025-05-21 21:19:05
+// Description : Full Training 
+// Problem Name: G_Yarab_E_3_fa
+// Learned     : none
+//=====================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int n;cin>>n;
+    for(int i = 0 ; i < n; i++){
+        cout <<"Yarab Mahmoud Ayman Ya5od E3fa" << '\n';
+    }
+
+    return 0;
+}
