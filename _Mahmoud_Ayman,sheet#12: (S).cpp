97f6ae0e1e092598bc80106595e589761969e512
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-07-01 22:39:46 +0300

    Create _Mahmoud_Ayman,sheet#12: (S).cpp

---
+++ _Mahmoud_Ayman,sheet#12: (S).cpp
@@ -0,0 +1,23 @@
+//=====================================================
+// Author      : ahmedgbr367
+// Date        : 2025-07-01 22:29:48
+// Problem Name: S_Goooooooooal
+// Learned     : 
+//=====================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    char s;
+    ll ans=0;
+    while(cin >> s){
+        if(s == 'o'){
+            ans++;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
