b4a10403129054402faed47f5693fc1317aa857c
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-07-28 23:54:16 +0300

    Create Assiut,sheet#1: (M).cpp

---
+++ Assiut,sheet#1: (M).cpp
@@ -0,0 +1,20 @@
+    // Online C++ compiler to run C++ program online
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main() {
+          char x;
+          cin >>x;
+          if(x >= '0' && x <='9'){
+          cout << "IS DIGIT";
+          }
+          else {
+              if(x>='a' && x<='z'){
+                  cout<<"ALPHA"<<endl<<"IS SMALL";
+              }
+              else{
+                  cout<<"ALPHA"<<endl<<"IS CAPITAL";
+              }
+          }
+    }
