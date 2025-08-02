7ab5027004fe8d5854662643479c92935c4ee4b0
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-08-02 23:55:00 +0300

    Create Assiut,sheet#1: (P).cpp

---
+++ Assiut,sheet#1: (P).cpp
@@ -0,0 +1,17 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main(){
+        short x;
+        cin >> x;
+     
+        if( (x/1000) % 2 == 0){
+            cout << "EVEN";
+        }
+        else{
+            cout << "ODD";
+        }
+     
+     
+        return 0;
+    }
