b53c9087b6d3a71feef301cc88d81174109d3c80
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-08-29 22:57:20 +0300

    Create Assiut,sheet#1: (A).cpp

---
+++ Assiut,sheet#1: (A).cpp
@@ -0,0 +1,10 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    //hi
+    int main(){
+        string s;
+        cin >> s;
+        cout << "Hello, " << s;
+      
+        return 0;
+    }
