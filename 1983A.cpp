1a85070fd9abbe858d7ae637edea00cb0197e308
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-11-12 23:53:33 +0200

    Create 1983A.cpp

---
+++ 1983A.cpp
@@ -0,0 +1,28 @@
+    //=====================================================
+    // Author      : ahmedgbr367
+    // Date        : 2024-07-07 17:45:42
+    // Description : Junior Training Sheet V7.0
+    // Problem Code: 1983A
+    // Problem Name: A_Array_Divisibility
+    // Learned     : 
+    //=====================================================
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main(){
+        cin.tie(0)->sync_with_stdio(0);
+        int t;
+        cin >> t ; 
+        for (int i = 0 ; i< t ; i ++){
+            int n;
+            cin >> n;
+            for (int i =1 ; i<=n;i++){
+                cout << i << " ";
+            }
+            cout << endl;
+     
+        }
+     
+     
+        return 0;
+    }
