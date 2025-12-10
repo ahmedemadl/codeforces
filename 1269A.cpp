769c7cb851b324e6b487db3fb8fb9903f7fe92a6
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-12-10 21:58:57 +0200

    Create 1269A.cpp

---
+++ 1269A.cpp
@@ -0,0 +1,16 @@
+    //=====================================================
+    // Author      : ahmedgbr367
+    // Date        : 2025-12-10 21:28:04
+    // Problem Name: A_Equation
+    // Note        : 
+    //=====================================================
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+     
+    int main(){
+        cin.tie(0)->sync_with_stdio(0);
+        int n;cin >> n;
+        cout << 9*n << " " << 8*n;
+        return 0;
+    }
