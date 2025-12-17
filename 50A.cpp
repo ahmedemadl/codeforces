6d26d9034da11a15bb4008adac915064bd4517e4
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-12-17 23:25:34 +0200

    Create 50A.cpp

---
+++ 50A.cpp
@@ -0,0 +1,16 @@
+//=====================================================
+// Author      : ahmedgbr367
+// Date        : 2025-12-17 23:24:03
+// Problem Name: A_Domino_piling
+// Note        : 
+//=====================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int a,b ; cin >> a >> b;
+    cout << a * b / 2;
+    return 0;
+}
