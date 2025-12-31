6fbf8eb12acc2e0bc176a8454944b4bf15be7959
Ahmed Emad <124065753+ahmedemadl@users.noreply.github.com>
2025-12-31 21:59:53 +0200

    Create 509A.cpp

---
+++ 509A.cpp
@@ -0,0 +1,29 @@
+//=====================================================
+// Author      : ahmedgbr367
+// Date        : 2025-05-23 17:40:04
+// Problem Name: J - Maximum in Table
+// Learned     : 
+//=====================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int n;cin>>n;
+    int a[n][n];
+    for(int i = 0 ; i < n ; i++){
+        a[i][0]=1;
+        a[0][i]=1;
+    }
+    for(int i = 1 ; i < n ; i ++){
+        for(int j = 1 ; j < n; j++){
+            a[i][j]= a[i-1][j] + a[i][j-1];
+        }
+    }
+
+
+    cout << a[n-1][n-1];
+
+    return 0;
+}
